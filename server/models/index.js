38628fd76e306be163caad36b90237688b7ca15b
Vaishnavi Nalawade <vaishnavinalawade2024@gmail.com>
2026-09-05 18:05:31 +0000

    updated index.js file

--- server/models/index.js
+++ server/models/index.js
@@ -7,9 +7,7 @@ const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: process.env.DB_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: false
 });
 
 console.log('database connected')
